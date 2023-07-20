a8ded6c9cd037738598be300ab8b3e026b3e56c0
Precella <marketing.katimas@gmail.com>
2023-07-20 08:37:36 +0800

    first commit

---
+++ assets/js/whyChooseUsSlider.js
@@ -0,0 +1,13 @@
+// Testing Using Owl-Carousel Here
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        smartSpeed: 1500,
+        autoplayHoverPause: true
+    });
+});
